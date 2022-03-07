f93333973d501dcdf09245d9ca094b7947846990
jaydev <jaydev@betteryou.ai>
2022-03-07 11:20:44 -0600

    Initial commit

---
+++ src/database/FirebaseDefault.js
@@ -0,0 +1,15 @@
+// import crashylytics from '@react-native-firebase/crashlytics';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+// import functions from '@react-native-firebase/functions';
+// import { firebase } from '@react-native-firebase/storage';
+
+/**
+ * Firebase default entry point
+ * */
+
+// export const fire_crash = crashylytics();
+export const fstore = firestore();
+// export const cloud_functions = functions();
+export const fire_auth = auth();
+// export const firebase_storage = firebase;
