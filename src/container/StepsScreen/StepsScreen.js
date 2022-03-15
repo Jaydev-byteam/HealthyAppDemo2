a4c611bb518094342080a9a70d715aca75c5d2ec
jaydev <jaydev@betteryou.ai>
2022-03-15 10:38:52 -0500

    Basic steps screen layout with hardcoded info complete

--- src/container/StepsScreen/StepsScreen.js
+++ src/container/StepsScreen/StepsScreen.js
@@ -1,25 +1,41 @@
 import * as React from 'react';
-import {TouchableOpacity, View, Text} from 'react-native';
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  Image,
+} from 'react-native';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import PageTitle from '../../components/PageTitle/PageTitle';
 import styles from './StepsScreenStyles';
-// import Icon from 'react-native-vector-icons/FontAwesome';
+import {ProgressCircle} from 'react-native-svg-charts';
+import images from '../../../assets/images';
 
 const weeklyAverageSteps = 0;
 const stepsGoal = 5000;
-const dailySteps = 0;
+const dailySteps = 3456;
 
 export default function StepsScreen({navigation}) {
   return (
     <View style={styles.container}>
-      <KeyboardAwareScrollView >
+      <KeyboardAwareScrollView>
         <PageTitle pageName={'Hit Daily Steps'} showIcon={false} />
-        <Text style={styles.goalAmount}>
-          Goal: {stepsGoal} steps/day
-        </Text>
-        <Text style={styles.dailySteps}>
-          Daily Steps: {dailySteps}
-        </Text>
+        <Text style={styles.goalAmount}>Goal: {stepsGoal} steps/day</Text>
+        <Text style={styles.dailySteps}>Daily Steps: {dailySteps}</Text>
+        <View style={styles.card}>
+          <ProgressCircle
+            style={styles.progress}
+            progress={dailySteps/stepsGoal}
+            progressColor={'aqua'}
+            backgroundColor={'white'}
+            strokeWidth={12}
+          />
+
+          <Image style={styles.logo} source={images.stepsIcon} />
+        </View>
+        <Text style={styles.dailySteps}>{weeklyAverageSteps} steps/day</Text>
+        <Text style={styles.goalAmount}>(Average of last 7 days)</Text>
       </KeyboardAwareScrollView>
     </View>
   );
--- src/container/StepsScreen/StepsScreenStyles.js
+++ src/container/StepsScreen/StepsScreenStyles.js
@@ -18,6 +18,27 @@ export default StyleSheet.create({
     textAlign: 'center',
     fontWeight: 'bold',
     fontSize: 14,
-  }
+  },
+  card: {
+    flex: 1,
+    alignItems: 'center',
+    height: 200,
+    width: 200,
+    marginVertical: 20,
+  },
+  progress: {
+    height: 160,
+    width: 160,
+    position: 'absolute',
+    top: 0,
+  },
+  logo: {
+    width: 120,
+    height: 120,
+    marginTop: 20,
+    // marginLeft: 20,
+    borderRadius: 60,
+    resizeMode: 'contain',
+  },
 
 });
