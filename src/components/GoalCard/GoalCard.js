055cc78be53ac4b3a12571bbd7c72af05d3c8838
jaydev <jaydev@betteryou.ai>
2022-03-23 16:24:36 -0500

    Changed props for GoalCard to destructured explicit props

--- src/components/GoalCard/GoalCard.js
+++ src/components/GoalCard/GoalCard.js
@@ -2,29 +2,36 @@ import React from 'react';
 import {Text, Image, View, TouchableWithoutFeedback} from 'react-native';
 import {ProgressCircle} from 'react-native-svg-charts';
 import styles from './GoalCardStyles';
-import { styleConstants } from "../../_constants/StyleConstants";
+import {styleConstants} from '../../_constants/StyleConstants';
 import images from '../../../assets/images/';
 
 // create a component for the Goal Card to be shown on the Goals Screen
-export default function GoalCard(props) {
+export default function GoalCard({
+  image,
+  goalTitle,
+  goalAmount,
+  goalUnit,
+  goalProgress,
+  navigation,
+  destination,
+}) {
   return (
     <View style={styles.card}>
       <ProgressCircle
         style={styles.progress}
-        progress={props.goalProgress}
+        progress={goalProgress}
         progressColor={styleConstants.progress_color}
         backgroundColor={'none'}
         strokeWidth={10}
       />
       <TouchableWithoutFeedback
-        onPress={() => props.navigation.navigate(props.destination)}
-      >
-        <Image style={styles.logo} source={props.image} />
+        onPress={() => navigation.navigate(destination)}>
+        <Image style={styles.logo} source={image} />
       </TouchableWithoutFeedback>
       <View style={styles.cardInfo}>
-        <Text style={styles.goalTitle}>{props.goalTitle}</Text>
+        <Text style={styles.goalTitle}>{goalTitle}</Text>
         <Text style={styles.goalAmount}>
-          {props.goalAmount} {props.goalUnit}
+          {goalAmount} {goalUnit}
         </Text>
       </View>
     </View>
