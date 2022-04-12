2be5f4b7cf254a79432518f52b4c8bd9b625056a
jaydev <jaydev@betteryou.ai>
2022-04-12 14:54:42 -0500

    Revised EditSleepGoal fonts, culled dead code

--- src/components/EditSleepGoal/EditSleepGoalStyles.js
+++ src/components/EditSleepGoal/EditSleepGoalStyles.js
@@ -1,6 +1,6 @@
 import {StyleSheet} from 'react-native';
-import { styleConstants } from "../../_constants/StyleConstants";
-import {fontConstants} from "../../_constants/FontConstants";
+import {styleConstants} from '../../_constants/StyleConstants';
+import {fontConstants} from '../../_constants/FontConstants';
 
 export default StyleSheet.create({
   sleepEdit: {
@@ -32,7 +32,6 @@ export default StyleSheet.create({
     height: 40,
     width: 40,
     borderRadius: 20,
-
   },
   addButton: {
     justifyContent: 'center',
@@ -52,33 +51,6 @@ export default StyleSheet.create({
     fontSize: 12,
     fontFamily: fontConstants.button,
   },
-  // title: {
-  //   color: styleConstants.light_text,
-  //   fontWeight: 'bold',
-  //   fontSize: 16,
-  //   marginVertical: 5,
-  // },
-  // bedtime: {
-  //   color: styleConstants.light_text,
-  //   fontWeight: 'bold',
-  //   fontSize: 20,
-  //   marginVertical: 5,
-  // },
-  // editBar: {
-  //   flex: 1,
-  //   flexDirection: 'row',
-  //   justifyContent: 'space-around',
-  //   alignItems: 'center',
-  //   backgroundColor: 'none',
-  // },
-  // button: {
-  //   backgroundColor: styleConstants.background_color,
-  // },
-  // note: {
-  //   color: styleConstants.light_text,
-  //   fontWeight: 'bold',
-  //   fontSize: 16,
-  // },
   datePicker: {
     backgroundColor: styleConstants.light_background,
     width: 300,
@@ -87,19 +59,4 @@ export default StyleSheet.create({
     alignItems: 'center',
     marginTop: 10,
   },
-  // editBar: {
-  //   flex: 1,
-  //   flexDirection: 'row',
-  //   justifyContent: 'space-around',
-  //   alignItems: 'center',
-  //   backgroundColor: 'none',
-  // },
-  // button: {
-  //   backgroundColor: styleConstants.background_color,
-  // },
-  // note: {
-  //   color: styleConstants.light_text,
-  //   fontWeight: 'bold',
-  //   fontSize: 16,
-  // },
 });
